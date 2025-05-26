42d712bfbf9b35903cc8604b66f2db6c54f131b8
HenkVanAsselt <henk.van.asselt@gmail.com>
2025-05-26 14:43:46 +0200

    Many changes... One can select the USB device to use. A scan result is converted to the AIM identifier and the barcode data.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use hidapi::{DeviceInfo, HidApi};
+use std::io;
 
 fn enumerate_usb_devices() -> Vec<DeviceInfo> {
     let mut unique_ids: Vec<String> = Vec::new();
@@ -46,15 +47,39 @@ fn enumerate_usb_devices() -> Vec<DeviceInfo> {
     available_usb_devices
 }
 
+fn select_usb_device(devices: Vec<DeviceInfo>) -> usize {
+    show_available_devices(devices.clone());
+
+    // Prompt the user for an index
+    println!("Please enter the index of the item you want to use:");
+
+    // Read the user input
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // Parse the input to an integer
+    let index: usize = input.trim().parse().unwrap();
+    let max_index = devices.len();
+    let i = if index >= 0 && index < max_index {
+        index
+    } else {
+        println!("Invalid index. Please try again.");
+        select_usb_device(devices)
+    };
+    i
+}
+
 fn show_available_devices(devices: Vec<DeviceInfo>) {
-    for device in devices {
+    for (index, device) in devices.iter().enumerate() {
         let manufacturer_string = device.manufacturer_string().unwrap();
         let product_string = device.product_string().unwrap();
         let vendor_id = device.vendor_id();
         let product_id = device.product_id();
         println!(
-            "{:20} {:40} ({:04x}:{:04x})",
-            product_string, manufacturer_string, vendor_id, product_id
+            "{}: {:20} {:40} ({:04x}:{:04x})",
+            index, product_string, manufacturer_string, vendor_id, product_id
         );
     }
 }
@@ -62,16 +87,23 @@ fn show_available_devices(devices: Vec<DeviceInfo>) {
 fn main() {
     println!("All available hid devices:");
     let available_usb_devices: Vec<DeviceInfo> = enumerate_usb_devices();
-    show_available_devices(available_usb_devices);
+    // show_available_devices(available_usb_devices.clone());
+    let index = select_usb_device(available_usb_devices.clone());
+    println!("Selected device: {:?}", available_usb_devices[index]);
+    
+    let device = &available_usb_devices[index];
 
     // Interaction:
 
     // // Initialize the HID API
     let api = HidApi::new().unwrap();
 
-    // This is for the Honeywell 1602g
-    let vendor_id = 0x0c2e; // Example vendor ID
-    let product_id = 0x0db3; // Example product ID
+    // // This is for the Honeywell 1602g
+    // let vendor_id = 0x0c2e; // Example vendor ID
+    // let product_id = 0x0db3; // Example product ID
+    
+    let vendor_id = device.vendor_id();
+    let product_id = device.product_id();
 
     // Open the device
     let device = api
@@ -80,33 +112,42 @@ fn main() {
     println!("Opened device: {:#?}", device.get_product_string().unwrap().unwrap());
     println!();
 
-    // let manuf_str = device.get_manufacturer_string();
-    // println!("Manufacturer string: {:?}", manuf_str);
-    // let prod_str = device.get_product_string();
-    // println!("Product string: {:?}", prod_str);
-
     // Example: Write data to the device
-    let command = [0xFD, 0x03, 0x16, 0x54, 0x0d]; // Replace with your data
+    let command = [0xFD, 0x03, 0x16, 0x54, 0x0d]; // Scanner Trigger on
     let result = device.write(&command);
     match result {
-        Ok(_) => println!("Command sent successfully!"),
+        Ok(_) => println!("TRIGGER ON Command sent successfully!"),
         Err(e) => eprintln!("Failed to send command: {}", e),
     }
 
-    // // Example: Read data from the device
+    // Read data from the device
     let mut buf = [0u8; 256]; // Buffer to hold the read data
     let bytes_read = device.read(&mut buf[..]).unwrap();
-    let mut data_string = String::new();
-    for u in &buf[..bytes_read] {
-        data_string.push_str(&(u.to_string() + "\t"));
-    }
+    println!("Raw data: {:?}", buf);
+    // let mut aim_identifier = String::new();
+    let aim_identifier: String = buf[2..=4].iter()
+        .map(|&x| x as u8 as char) // Convert each integer to a char
+        .collect(); // Collect the characters into a String
+    println!("AIM identifier: {}", aim_identifier);
+    
+    // let mut data_string = String::new();
+    // Convert the vector to a string. This skips the header AND the AIM identifier.
+    let data_string: String = buf[5..=bytes_read].iter()
+        .map(|&x| x as u8 as char) // Convert each integer to a char
+        .collect(); // Collect the characters into a String
+    println!("datastring: {}", data_string);
 
-    println!("{}", data_string);
-
-    let command = [0xFD, 0x03, 0x16, 0x55, 0x0d]; // Replace with your data
+    let command = [0xFD, 0x03, 0x16, 0x55, 0x0d]; // Trigger off
     let result = device.write(&command);
     match result {
-        Ok(_) => println!("Command sent successfully!"),
+        Ok(_) => println!("TRIGGER OFF Command sent successfully!"),
         Err(e) => eprintln!("Failed to send command: {}", e),
     }
+
+    let command = [0xFD, 0x03, 0x16, 0x07, 0x0d]; // Beep
+    let result = device.write(&command);
+    match result {
+        Ok(_) => println!("BEEP Command sent successfully!"),
+        Err(e) => eprintln!("Failed to send command: {}", e),
+    }    
 }
