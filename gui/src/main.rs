774cc66edf320a915841fa87fd7fcbb8a9ed4dc9
HenkVanAsselt <henk.van.asselt@gmail.com>
2025-05-29 11:46:40 +0200

    Starting developing with egui. Still have to move functionality to seperate files, so it can be used by both the CLI and the GUI.

---
+++ gui/src/main.rs
@@ -0,0 +1,72 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "HON Scanner Control", // This will be the name of the window
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Henk".to_owned(),
+            age: 61,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("HON Scanner Control");
+            if ui.button("DEFOVR.").clicked() {
+                println!("Sending DEFOVR.");
+            }
+            if ui.button("DEFALT.").clicked() {
+                println!("Sending DEFALT.");
+            }
+            if ui.button("REVINF.").clicked() {
+                println!("Sending REVINF.");
+            }
+            if ui.button("BEEP").clicked() {
+                println!("Sending a beep command");
+            }
+
+            // From the original example:
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            // ui.image(egui::include_image!(
+            //     "./ferris.png"
+            // ));
+        });
+    }
+}
